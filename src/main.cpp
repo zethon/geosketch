32c1dbbeda4034aeb98096fdb057eb00db5c53c6
Adalid Claure <aclaure@gmail.com>
2023-06-03 20:08:00 -0400

    force SFML to use shared libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
-#include <SFML/Graphics.hpp>
 #include <cassert>
+
+#include <boost/program_options.hpp>
+
+#include <SFML/Graphics.hpp>
+
+namespace po = boost::program_options;
 
 int main() 
 {
-    sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "Hello World SFML Window");
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,?", "print help message")
+        ("resources,r", po::value<std::string>(), "path of resource folder")
+        ("window-size,w", po::value<std::string>(), "window size")
+        ("version,v", "print version string")
+        ;
+
+
+    std::shared_ptr<sf::RenderWindow> win;
+    win = std::make_shared<sf::RenderWindow>(sf::VideoMode(1280, 720),
+        "Hello World SFML Window", sf::Style::Titlebar | sf::Style::Close);
+    win->setFramerateLimit(60);
     sf::Font font;
     if (!font.loadFromFile("/Users/addy/src/ttvg/resources/fonts/mono.ttf")) 
     {
@@ -11,25 +28,25 @@ int main()
     }
     sf::Text message("Hello, World !", font);
 
-    while (sfmlWin.isOpen()) 
+    while (win->isOpen())
     {
         sf::Event e;
-        while (sfmlWin.pollEvent(e)) 
+        while (win->pollEvent(e))
         {
 
             switch (e.type) 
             {
                 case sf::Event::EventType::Closed:
-                    sfmlWin.close();
+                    win->close();
                 break;
                 default:
                 break;
             }
         }
 
-        sfmlWin.clear();
-        sfmlWin.draw(message);
-        sfmlWin.display();
+        win->clear();
+        win->draw(message);
+        win->display();
     }
     return 0;
 }
