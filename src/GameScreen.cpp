dda693de3b8bada68268fc19a7675ebbe6ec159e
Addy <aclaure@gmail.com>
2023-08-05 09:34:54 -0400

    this colors things nicely for now;

--- src/GameScreen.cpp
+++ src/GameScreen.cpp
@@ -17,52 +17,27 @@ GameScreen::GameScreen(sf::RenderTarget& target, ResourceManager& resources)
     sf::Vector2f anchor{xloc, yloc};
     sf::Vector2f mapsize{edgelen, edgelen};
     
-    constexpr auto griddle = 100u;
+    constexpr auto griddle = 50u;
     sf::Vector2u gridsize{griddle, griddle};
     
     _tiles = std::make_unique<TileManager>(_target, anchor, mapsize, gridsize);
-//    constexpr auto RIGHT_BUFFER = 50u;
-//    constexpr auto LEFT_BUFFER = 100u;
-//    constexpr auto TOP_BUFFER = 20u;
-//    constexpr auto BOTTOM_BUFFER = 20u;
-//    
-//    const auto winsize = _target.getSize();
-//    const auto winwidth = winsize.x;
-//    const auto winheight = winsize.y;
-//    
-//    const auto edgelen = (winwidth > winheight ? winheight : winwidth) * 0.95;
-    
     auto outline = this->emplaceDrawable<sf::RectangleShape>(
         sf::Vector2f{static_cast<std::float_t>(edgelen), static_cast<std::float_t>(edgelen)});
-//    
-//    const auto xloc = (winwidth - (edgelen + (edgelen * 0.05)));
-//    const auto yloc = (winheight / 2) - (edgelen / 2);
     outline->setPosition(xloc, yloc);
     outline->setFillColor(sf::Color::Transparent);
     outline->setOutlineThickness(5.0f);
     outline->setOutlineColor(sf::Color(122, 122, 122));
-//    
-//    constexpr auto griddle = 100u;
-//    const auto step = edgelen / griddle;
-//    for (auto x = step; x < edgelen; x+=step)
-//    {
-//        auto line = this->emplaceDrawable<sf::RectangleShape>(
-//            sf::Vector2f{static_cast<std::float_t>(1), static_cast<std::float_t>(edgelen)});
-//        line->setPosition(xloc+x, yloc);
-//        line->setFillColor(sf::Color{50,50,50});
-//    }
-//    
-//    for (auto y = step; y < edgelen; y+=step)
-//    {
-//        auto line = this->emplaceDrawable<sf::RectangleShape>(
-//            sf::Vector2f{static_cast<std::float_t>(edgelen), static_cast<std::float_t>(1)});
-//        line->setPosition(xloc, yloc+y);
-//        line->setFillColor(sf::Color{50,50,50});
-//    }
 }
 
-PollResult GameScreen::poll(const sf::Event&)
+PollResult GameScreen::poll(const sf::Event& e)
 {
+    if (const auto result = Screen::poll(e);
+        result.type != gs::ActionType::NONE)
+    {
+        return result;
+    }
+    
+    _tiles->event(e);
     return {};
 }
 
--- src/GameScreen.h
+++ src/GameScreen.h
@@ -2,14 +2,23 @@
 
 #include "Screen.h"
 #include "GeoSketchLogger.h"
+#include <cassert>
 
 namespace gs
 {
 
 class Tile : public sf::Drawable
 {
+    const sf::Color DEFAULT{sf::Color::Transparent};
+    const sf::Color HIGHLIGHT{sf::Color{150,150,150}};
+    const sf::Color SELECTED{sf::Color{0,100,0}};
     
 public:
+    enum class State
+    {
+        DEFAULT, HIGHLIGHT, SELECTED
+    };
+    
     Tile(const sf::Vector2f& location, const sf::Vector2f& size)
         : _color{sf::Color::Transparent}
     {
@@ -20,9 +29,22 @@ public:
         _rect.setOutlineThickness(0.5f);
     }
     
-    void setColor(const sf::Color& color)
+    void setState(Tile::State state)
     {
-        _rect.setFillColor(color);
+        switch (state)
+        {
+            default:
+                _rect.setFillColor(DEFAULT);
+            break;
+                
+            case State::HIGHLIGHT:
+                _rect.setFillColor(HIGHLIGHT);
+            break;
+                
+            case State::SELECTED:
+                _rect.setFillColor(SELECTED);
+            break;
+        }
     }
     
     void draw(sf::RenderTarget& target, sf::RenderStates states) const
@@ -44,23 +66,27 @@ public:
           _anchor{anchor},
           _mapSize{mapsize},
           _gridSize{gridsize},
-          _logger{ log::initializeLogger("TileManager") }
+          _logger{ log::initializeLogger("TileManager") },
+          _xmin{_anchor.x},
+          _xmax{_anchor.x + _mapSize.x},
+          _ymin{_anchor.y},
+          _ymax{_anchor.y + _mapSize.y},
+          _tileWidth{_mapSize.x / _gridSize.x},
+          _tileHeight{_mapSize.y / _gridSize.y}
     {
         _logger->trace("Anchor: {},{}", anchor.x, anchor.y);
         _logger->trace("MapSize: {},{}", mapsize.x, mapsize.y);
         _logger->trace("GridSize: {},{}", gridsize.x, gridsize.y);
-        const auto tileWidth = mapsize.x / gridsize.x;
-        const auto tileHeight = mapsize.y / gridsize.y;
-        const sf::Vector2f tileSize{ tileWidth, tileHeight };
 
+        const sf::Vector2f tileSize{ _tileWidth, _tileHeight };
         for (auto xidx = 0u; xidx < gridsize.x; xidx++)
         {
             _tileContainer.push_back(std::vector<std::shared_ptr<Tile>>{});
-            float x = _anchor.x + (xidx * tileWidth);
+            float x = _anchor.x + (xidx * _tileWidth);
             auto& back = _tileContainer.back();
             for (auto yidx = 0u; yidx < gridsize.y; yidx++)
             {
-                float y = _anchor.y + (yidx * tileHeight);
+                float y = _anchor.y + (yidx * _tileHeight);
                 const sf::Vector2 loc{x, y};
                 auto tile = std::make_shared<Tile>(loc, tileSize);
                 back.push_back(tile);
@@ -79,14 +105,52 @@ public:
         }
     }
     
-    void event(const sf::Event& e)
+    void event(const sf::Event& ev)
     {
-        
+        switch (ev.type)
+        {
+            default:
+            break;
+                
+            case sf::Event::MouseMoved:
+            {
+                if (ev.mouseMove.x < _xmin || ev.mouseMove.x > _xmax || ev.mouseMove.y < _ymin || ev.mouseMove.y > _ymax) return;
+
+                const auto local = this->getXYCords(sf::Vector2i{ev.mouseMove.x, ev.mouseMove.y});
+                if (auto ptr = _tileContainer[local.x][local.y]; _lastTile.get() != ptr.get())
+                {
+                    if (_lastTile != nullptr)
+                    {
+                        _lastTile->setState(Tile::State::DEFAULT);
+                    }
+
+                    _tileContainer[local.x][local.y]->setState(Tile::State::HIGHLIGHT);
+                    _lastTile = _tileContainer[local.x][local.y];
+                    _logger->trace("mouse: {}, {}", ev.mouseMove.x, ev.mouseMove.y);
+                    _logger->trace("tile: {},{}", local.x, local.y);
+                }
+            }
+            break;
+        }
     }
     
 private:
-    sf::RenderTarget&   _window;
+    sf::Vector2u getXYCords(const sf::Vector2i& mouseCord)
+    {
+        const auto localX = std::max(0.0f, mouseCord.x - _anchor.x);
+        const auto localY = std::max(0.0f, mouseCord.y - _anchor.y);
+        
+        const std::uint32_t xcord = std::min(static_cast<std::uint32_t>(std::floor(localX / _tileWidth)), _gridSize.x - 1);
+        const std::uint32_t ycord = std::min(static_cast<std::uint32_t>(std::floor(localY / _tileHeight)), _gridSize.y - 1);
+        
+        assert(xcord < _tileContainer.size());
+        assert(ycord < _tileContainer[xcord].size());
+
+        return {xcord, ycord};
+    }
     
+    sf::RenderTarget&   _window;
+
     sf::Vector2f        _anchor;
     sf::Vector2f        _mapSize;
     sf::Vector2u        _gridSize;
@@ -95,6 +159,17 @@ private:
     TileContainer       _tileContainer;
     
     log::SpdLogPtr      _logger;
+    
+    // const properties
+    const std::float_t        _xmin;
+    const std::float_t        _xmax;
+    const std::float_t        _ymin;
+    const std::float_t        _ymax;
+    const std::float_t        _tileWidth;
+    const std::float_t        _tileHeight;
+    
+    std::shared_ptr<Tile>     _lastTile = nullptr;
+    
 };
 
 class GameScreen : public Screen
